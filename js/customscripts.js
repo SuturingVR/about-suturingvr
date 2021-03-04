78d096b85223373c1897ad68ef6ad7a850846273
KingScroll <69558037+KingScroll@users.noreply.github.com>
2021-03-04 17:34:00 -0500

    Push 2 -- Images & Pretty Stuff :)

---
+++ js/customscripts.js
@@ -0,0 +1,10 @@
+function onHover()
+{
+    console.log("Switch 2 White");
+    $("#badge-link-img").attr('src', '/img/Oculus-Badge-White.png');
+}
+
+function offHover()
+{
+    $("#badge-link-img").attr('src', '/img/Oculus-Badge.png');
+}
